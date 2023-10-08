105b3b2242910e296a411443fefeb37c1e314560
麟 <976791636@qq.com>
2023-10-08 17:19:31 +0800

    update

--- admin/src/data/index.js
+++ admin/src/data/index.js
@@ -1,7 +1,8 @@
 import { getItem } from '@/utils/store'
 
-export const domain = '127.0.0.1:8000'
-export const http = 'http://' + domain
+export const domain = 'gmood.site'
+// export const domain = '127.0.0.1:8000'
+export const http = 'https://' + domain
 export const pageNum = 10
 
 const initData = ()=>{
