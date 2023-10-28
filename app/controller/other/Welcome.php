7bf61d5638ef6bf4c1f70da530887f87ac471c85
root <root@C20230219177953.local>
2023-10-28 16:46:19 +0000

    updae

--- app/controller/other/Welcome.php
+++ app/controller/other/Welcome.php
@@ -9,12 +9,19 @@ class Welcome extends Base
     function index(){
         // return self::ResData($_SERVER);
         return self::Success('获取成功',['data'=>[
-            ['name'=>'服务器操作系统','val'=>$_SERVER['HTTP_SEC_CH_UA_PLATFORM']],
-            ['name'=>'服务器软件','val'=>$_SERVER['SERVER_SOFTWARE']],
+            ['name'=>'服务器系统类型','val'=>php_uname('s')],
+            ['name'=>'服务器系统版本号','val'=>php_uname('r')],
+            ['name'=>'服务器解译引擎','val'=>$_SERVER['SERVER_SOFTWARE']],
+            ['name'=>'服务器语言','val'=>$_SERVER['HTTP_ACCEPT_LANGUAGE']],
+            ['name'=>'服务器地址','val'=>$_SERVER['SERVER_ADDR']],
             ['name'=>'服务器端口','val'=>$_SERVER['SERVER_PORT']],
-            ['name'=>'服务器地址','val'=>$_SERVER['SERVER_NAME']],
-            ['name'=>'服务器软件','val'=>$_SERVER['SERVER_SOFTWARE']],
-            ['name'=>'服务器软件','val'=>$_SERVER['SERVER_SOFTWARE']],
+            ['name'=>'域名','val'=>$_SERVER['SERVER_NAME']],
+
+            ['name'=>'PHP版本','val'=>PHP_VERSION],
+            ['name'=>'PHP运行方式','val'=>php_sapi_name()],
+            ['name'=>'Zend版本','val'=>Zend_Version()],
+
+            ['name'=>'当前登录IP','val'=>$_SERVER['REMOTE_ADDR']],
         ]]);
     }
 }
