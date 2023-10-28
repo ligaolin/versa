35de6e637c8a3c570b564c3e3b92aa5da6f529ff
麟 <976791636@qq.com>
2023-10-29 06:31:55 +0800

    update

--- admin/src/data/index.js
+++ admin/src/data/index.js
@@ -1,4 +1,4 @@
-// export const domain = 'gmood.site'
-export const domain = '127.0.0.1:8000'
-export const http = 'http://' + domain
+export const domain = 'gmood.site'
+// export const domain = '127.0.0.1:8000'
+export const http = 'https://' + domain
 export const pageNum = 10
